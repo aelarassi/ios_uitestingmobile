c4255ced1bfbfc46ac2fccf7198a0cf9381198af
ABDELAZiZ EL ARASSi <hi@aelarassi.com>
2024-05-09 15:08:03 +0100

    Initial Commit

---
+++ UITestingDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITestingDemo
+//
+//  Created by ABDELAZiZ EL ARASSi on 9/5/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
