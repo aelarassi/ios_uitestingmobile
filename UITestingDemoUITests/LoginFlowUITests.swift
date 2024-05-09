a798147eacafea42401a53c5f778ce87d6cbba59
ABDELAZiZ EL ARASSi <hi@aelarassi.com>
2024-05-09 18:01:34 +0100

    Screenshot of element after typing a content and saved in "Jump to Report "

--- UITestingDemoUITests/LoginFlowUITests.swift
+++ UITestingDemoUITests/LoginFlowUITests.swift
@@ -13,24 +13,36 @@ final class LoginFlowUITests: XCTestCase {
         continueAfterFailure = false
     }
 
-    override func tearDownWithError() throws {
-    }
+    override func tearDownWithError() throws {}
 
     func testSignupFlowUITests_WhenTapped_PresentsRegisterViewController() throws {
+        
         let app = XCUIApplication()
         app.launch()
         
-        app.buttons["CreateAccountButton"].tap()
+        let emailField = app.textFields["emailField"]
+        emailField.tap()
+        emailField.typeText("abdel@aziz.ae")
+        
+        let emailFieldScreenshot = emailField.screenshot()
+        let emailFieldAttachement = XCTAttachment(screenshot: emailFieldScreenshot)
+        emailFieldAttachement.name = "Screenshot of Email UITextField"
+        emailFieldAttachement.lifetime = .keepAlways
+        add(emailFieldAttachement)
+        
+        let createAccountButton = app.buttons["CreateAccountButton"]
+        createAccountButton.tap()
         
         XCTAssertTrue(app.otherElements["RegisterViewController"].waitForExistence(timeout: 1), "RegisterViewController not exist")
+        
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
