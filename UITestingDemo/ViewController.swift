3c92c1eb6058b968f4710c6a170cdba5cd81cf21
ABDELAZiZ EL ARASSi <hi@aelarassi.com>
2024-05-09 15:54:44 +0100

    Create UI Register form

--- UITestingDemo/ViewController.swift
+++ UITestingDemo/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldFirstName: UITextField!
+    @IBOutlet weak var textFieldLastName: UITextField!
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    @IBOutlet weak var textFieldRepeatPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func actionButtonSend(_ sender: Any) {
+        print("Send!")
+    }
+    
 }
 
